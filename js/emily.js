2bd0c90672a6a13407b658e2fb651c5e3281ad49
Emilybmeyer <eaa23ebm@students.eaaa.dk>
2024-04-15 11:23:44 +0200

    emily

--- js/emily.js
+++ js/emily.js
@@ -1,12 +1,16 @@
 "use strict";
-fetch('../json/cases.json')
-  .then(response => response.json())
-  .then(data => {
-    const kristina = data[0];
-    const kristinaCitat = document.querySelector('.citat');
-    kristinaCitat.textContent = kristina.citat;
+// Wait for the JSON data to be loaded
+window.addEventListener('load', () => {
+  // Get the JSON data from the script tag
+  const jsonData = document.getElementById('json-data').textContent;
+  // Parse the JSON data
+  const data = JSON.parse(jsonData);
+  
+  // Get the kristina-citat and kristina-beskrivelse from the data
+  const kristinaCitat = data[0].kristina-citat;
+  const kristinaBeskrivelse = data[0].kristina-beskrivelse;
 
-    const kristinaVideo = document.querySelector('.kristina-mp4');
-    kristinaVideo.src = kristina.mp4;
-  })
-  .catch(error => console.error(error));
+  // Set the text content of the kristina-citat and kristina-beskrivelse elements
+  document.querySelector('.kristina-citat').textContent = kristinaCitat;
+  document.querySelector('.kristina-beskrivelse').textContent = kristinaBeskrivelse;
+});
