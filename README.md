e305e0bbff761c7190196eab4478bd2c2c1ffd6d
linelejel <eaa23llkk@students.eaaa.dk>
2024-04-16 11:07:02 +0200

    readme tekst og fil ændring

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ Derefter har vi defineret en funktion kaldet ''typeWriter()'', der  tilføjer hv
 
 Herefter tilføjer den så én karakter ad gangen til vores element ved at opdatere vores ''innerHTML''. Det der giver effekten er brug af ''setTimeout(), som gør at funktion kalder sig selv med en lille forsinkelse, så det ligner at teksten bliver skrevet i sekundet man ser det.
 
+
+<!-- forklaring af javascript kode til stemmesiden -->
+Forklaring af koden i antalclick.js siden
+
+Koden definere to variabler “clickCount og “newImages”. “ClikCount er et array med 3 elementer som skal vise klikantal for hvert billede. 
+“newImages” er ligeledes også et array med 3 stier til nye billeder der skal erstatte de “originale” 3 billeder, når der sker et click på et af billederne. 
+
+Funktionen “handleClick” håndterer klik på et billede, dvs. at når der klikkes på et billede, øges klikantallet for den valgte billede, og teksten opdateres efter det nye klikantal. Denne nye klikantal gemmes i den lokale lagring og billederne bliver erstattet med de nye billeder. Teksten med antal click/“stemmer” vises til sidst på hvert billede. 
+
+Til sidst hentes klikantallet fra den lokale lagring og vises i teksten for hvert billede, således at når siden opdateres, bliver antal click gemt og vises for hvert billede. 
--- js/antalclick.js
+++ js/antalclick.js
@@ -4,21 +4,35 @@
 	let clickCount = [0, 0, 0];
 	let newImages = ["img/kristina-hjerte.webp", "img/gustav-hjerte.webp", "img/nina-hjerte.webp"];
 
+    // funktion til at håndtere klik på et billede
 	function handleClick(imageNumber) {
+
+        // øg klikantallet for det valgte billede
 		clickCount[imageNumber - 1]++;
+
+        // opdater teksten med det nye klikantal
 		document.getElementById(`text${imageNumber}`).textContent = `${clickCount[imageNumber - 1]} stemmer`;
 		document.getElementById(`text${imageNumber}`).style.visibility = 'visible';
 
-		// Store click count in local storage
+		// Lagre klikantal i lokal lagring
 		localStorage.setItem(`clickCount${imageNumber}`, clickCount[imageNumber - 1]);
 
+        // opdater billederne med de nye billeder
 		document.getElementById("kristina-hjerte-outline").src = newImages[0];
 		document.getElementById("gustav-hjerte-outline").src = newImages[1];
 		document.getElementById("ninna-hjerte-outline").src = newImages[2];
+
+         // hvis tekst med antal stemmer for hvert billede
+         for (let i = 0; i < clickCount.length; i++) {
+        document.getElementById(`text${i + 1}`).textContent = `${clickCount[i]} stemmer`;
+        document.getElementById(`text${i + 1}`).style.visibility = "visible";
+    }
 	}
 
-	// Retrieve click count from local storage
+	// Hent klikantal fra lokal lagring
 	for (let i = 0; i < clickCount.length; i++) {
 		clickCount[i] = localStorage.getItem(`clickCount${i + 1}`) || 0;
 		document.getElementById(`text${i + 1}`).textContent = `${clickCount[i]} stemmer`;
 	}
+
+
